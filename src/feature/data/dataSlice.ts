89c26118f6fe859964812a648086e8dfaaa0b387
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-12-05 11:03:59 +0500

    feat: add createAsyncThunk and extraReducers for fetching data in store

--- src/feature/data/dataSlice.ts
+++ src/feature/data/dataSlice.ts
@@ -1,13 +1,24 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { ICharacter } from "../../types/dataType";
 
-interface Item {
+interface ItemState {
   items: ICharacter[];
+  status: "idle" | "loading" | "succeeded" | "failed";
 }
 
-const initialState: Item = {
+const initialState: ItemState = {
   items: [],
+  status: "idle",
 };
+
+export const fetchItems = createAsyncThunk("data/fetchItems", async () => {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.json();
+  return data.results as ICharacter[];
+});
 
 const dataSlice = createSlice({
   name: "data",
@@ -22,6 +33,22 @@ const dataSlice = createSlice({
     clearItems: (state) => {
       state.items = [];
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchItems.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(
+        fetchItems.fulfilled,
+        (state, action: PayloadAction<ICharacter[]>) => {
+          state.status = "succeeded";
+          state.items = action.payload;
+        }
+      )
+      .addCase(fetchItems.rejected, (state) => {
+        state.status = "failed";
+      });
   },
 });
 
